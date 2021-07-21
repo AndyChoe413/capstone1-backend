b05d4e2483e56bc10de75484fe8e24d9dc4300a8
Andy Choe <Andrew.s.choe@gmail.com>
2021-07-21 10:53:57 -0400

    commit 6

--- src/components/pages/settings/Settings.jsx
+++ src/components/pages/settings/Settings.jsx
@@ -3,9 +3,31 @@ import "./settings.css"
 
 export default function Settings() {
     return (
-        <div className="settings">
-            <div className="settingsWrapper"></div>
-            <Sidebar/>
-        </div>
-    )
+			<div className="settings">
+				<div className="settingsWrapper">
+					<div className="settingsTitle">
+						<span className="settingsUpdateTitle">Update your account</span>
+						<span className="settingsUpdateTitle">Delete Account</span>
+					</div>
+					<form className="settingsForm">
+						<label>Profile Picture</label>
+						<div className="settingsProfilePic">
+							<img src="" alt="" />
+							<label htmlFor="fileInput">
+								<i className="profilePicIcon far fa-user"></i>
+							</label>
+							<input type="file" id="fileInput" style={{ display: "none" }} />
+						</div>
+						<label>Username</label>
+						<input type="text" placeholder="Andy" />
+						<label>Username</label>
+						<input type="text" placeholder="Andy" />
+						<label>Username</label>
+						<input type="text" placeholder="Andy" />
+						<label>Username</label>
+					</form>
+				</div>
+				<Sidebar />
+			</div>
+		);
 }
